ebc2804c37082ff34fb591a00f4c25b8d4e3d08e
MB-study <MB-study@192.168.1.93>
2020-11-03 21:44:27 +0800

    java

---
+++ src/main/java/com/ocp/PrintStars.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ocp;
+
+/**
+ *
+ * @author MB-study
+ */
+public class PrintStars {
+
+    public static void main(String[] args) {
+        int line = 5;
+        /*
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+         */
+        for (int i = 1; i < line; i++) {
+            for (int j = 1; j <= j; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
