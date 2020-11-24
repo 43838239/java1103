73374b3817444807cb51a85760d9eb79f46cc813
MB-study <MB-study@192.168.1.93>
2020-11-24 20:06:01 +0800

    java

---
+++ src/main/java/com/ocp7/NumPlay.java
@@ -0,0 +1,14 @@
+
+package com.ocp7;
+
+
+public class NumPlay {
+    public static void main(String[] args) {
+        Num numl = new Num();
+        numl.runFiveTimes();
+        System.out.println(numl);
+        Num num2 = new Num();
+        num2.runFiveTimes();
+        System.out.println(num2);
+    }
+}
